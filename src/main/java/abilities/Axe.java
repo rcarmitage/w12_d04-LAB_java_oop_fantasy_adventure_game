4aafc51fbfd25994935762a01a255a7981f0aef8
Joe Cooper <josephcooper3@gmail.com>
2019-10-17 13:45:59 +0100

    cast IDefend in Axe attack method

--- src/main/java/abilities/Axe.java
+++ src/main/java/abilities/Axe.java
@@ -7,11 +7,15 @@ public class Axe implements IWeapon {
     private int damage;
 
     public Axe() {
-        this.damage = 1;
+        this.damage = 2;
     }
 
     public String attack(Enemy target) {
-        target.takeDamage(this.damage);
+        int damageToDeal = this.damage;
+        if (target instanceof IDefend) {
+            damageToDeal -= ((IDefend) target).defend();
+        }
+        target.takeDamage(damageToDeal);
         return "Swish Thwack!";
     }
 }
